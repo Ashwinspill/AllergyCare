235c756e9833f28999f2268e04d44d6bd686630d
Ashwinspill <118038285+Ashwinspill@users.noreply.github.com>
2023-10-10 23:00:49 +0530

    google sign 2

--- template/allapp/models.py
+++ template/allapp/models.py
@@ -1,26 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, role=None,dob=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, role=role,dob=dob, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+# class CustomUserManager(BaseUserManager):
+#     def create_user(self, email, password=None,dob=None, **extra_fields):     #, role='Patient'
+#         if not email:
+#             raise ValueError('The Email field must be set')
+#         email = self.normalize_email(email)
+#         user = self.model(email=email,dob=dob, **extra_fields)       # , role=role
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
 
-    def create_superuser(self, email, password=None, role='Admin',dob=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+#     def create_superuser(self, email, password=None, role='Admin',dob=None, **extra_fields):
+#         extra_fields.setdefault('is_staff', True)
+#         extra_fields.setdefault('is_superuser', True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
+#         if extra_fields.get('is_staff') is not True:
+#             raise ValueError('Superuser must have is_staff=True.')
+#         if extra_fields.get('is_superuser') is not True:
+#             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self.create_user(email, password, role=role,dob=dob, **extra_fields)
+#         return self.create_user(email, password, role=role,dob=dob, **extra_fields)
 
 class CustomUser(AbstractUser):
     ADMIN = 'Admin'
@@ -34,14 +34,17 @@ class CustomUser(AbstractUser):
 
     # Fields for custom user roles
     role = models.CharField(max_length=15, choices=ROLE_CHOICES, default=PATIENT)  # Default role for regular users
-    
+    role = models.CharField(max_length=15, choices=ROLE_CHOICES, default=DOCTOR)
+    forget_password_token = models.UUIDField(null=True, blank=True) #forgetpass
     email = models.EmailField(unique=True)
-    objects = CustomUserManager()
+    # objects = CustomUserManager()
     username = models.CharField(max_length=150, unique=True)
     dob = models.DateField(null=True, blank=True)
     # Define boolean fields for specific roles
-    is_patient = models.BooleanField(default=True)
-    is_doctor = models.BooleanField(default=False)
-
+    # is_patient = models.BooleanField(default=True)
+    # is_doctor = models.BooleanField(default=True)
+    is_patient=models.BooleanField('is_patient',default=False,null=True)
+    is_doctor=models.BooleanField('is_doctor',default=False,null=True)
+    REQUIRED_FIELDS=[]
     def __str__(self):
         return self.email
--- template/allapp/views.py
+++ template/allapp/views.py
@@ -1,50 +1,127 @@
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login as auth_login
-from django.contrib.auth import logout as auth_logout 
-# from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,login as auth_login, get_user_model
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from .models import CustomUser
 from .helpers import send_forget_password_mail
 from .forms import PatientProfileForm
+from django.views.decorators.cache import never_cache
+from django.contrib.auth.hashers import check_password
+from social_django.models import UserSocialAuth
+from social_django.utils import psa
+from django.views import View
+from django.contrib.auth.backends import ModelBackend
+
 
 # Create your views here.
+
 def index(request):
     return render(request,'index.html')
 
 # @login_required
+# def phome(request):
+#    if 'email' in request.session:
+#        response = render(request, 'phome.html')
+#        response['Cache-Control'] = 'no-store, must-revalidate'
+#        return response
+#    else:
+#        return redirect('logout_confirmation') 
+ 
+# def phome(request):
+#     if request.user.is_authenticated:
+#         response = render(request, 'phome.html')
+#         response['Cache-Control'] = 'no-store, must-revalidate'
+#         return response
+#     else:
+#         return redirect('logout_confirmation')
+#     # return render(request,'phome.html')
+@login_required
 def phome(request):
-   if 'username' in request.session:
-       response = render(request, 'phome.html')
-       response['Cache-Control'] = 'no-store, must-revalidate'
-       return response
-   else:
-       return redirect('logout_confirmation') 
-    # return render(request,'phome.html')
+    response = render(request, 'phome.html')
+    response['Cache-Control'] = 'no-store, must-revalidate'
+    return response    
+@login_required
+def dhome(request):
+    response = render(request, 'dhome.html')
+    response['Cache-Control'] = 'no-store, must-revalidate'
+    return response    
+
 # @login_required
 def appointment(request):
     return render(request,'appointment.html')
 def test(request):
     return render(request,'test.html')
+
+
+# #login with google and normal pakka code
+# def login(request):
+#     if request.method == 'POST':
+#         email = request.POST.get('email')
+#         password = request.POST.get('password')
+
+#         try:
+#             # Attempt to retrieve the user by email                                           #pakka sanm 
+#             user = CustomUser.objects.get(email=email)
+
+#             # Check the password
+#             if user and check_password(password, user.password):
+#                 auth_login(request, user, backend='django.contrib.auth.backends.ModelBackend')  # Specify the backend
+#                 return redirect('phome')
+#             else:
+#                 error_message = "Invalid credentials"
+#                 messages.error(request, error_message)
+#         except CustomUser.DoesNotExist:
+#             # User with the given email does not exist
+#             error_message = "User with this email does not exist"
+#             messages.error(request, error_message)
+
+#     response = render(request, 'login.html')
+#     response['Cache-Control'] = 'no-store, must-revalidate'
+#     return response
+
+    
+#testing new login
 def login(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
+    if request.method == 'POST':
+        email = request.POST.get('email')
         password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
+        try:
+            # Attempt to retrieve the user by email
+            user = CustomUser.objects.get(email=email)
 
-        if user is not None:
-            auth_login(request, user)
-            request.session['username']=username
-            return redirect("phome")
-        else:
-            messages.error(request, "Invalid login credentials")
-    response = render(request,'login.html')
+            # Check the password
+            if user and check_password(password, user.password):
+                auth_login(request, user, backend='django.contrib.auth.backends.ModelBackend')  # Specify the backend
+
+                # Redirect based on user's role and flags
+                if user.is_superuser:
+                    return redirect('admind')  # Redirect to admin dashboard
+                elif user.is_patient:
+                    return redirect('phome')  # Redirect to patient's page
+                elif user.is_doctor:
+                    return redirect('dhome')  # Redirect to doctor's page
+                else:
+                    error_message = "Invalid role"
+                    messages.error(request, error_message)
+
+            else:
+                error_message = "Invalid credentials"
+                messages.error(request, error_message)
+        except CustomUser.DoesNotExist:
+            # User with the given email does not exist
+            error_message = "User with this email does not exist"
+            messages.error(request, error_message)
+
+    response = render(request, 'login.html')
     response['Cache-Control'] = 'no-store, must-revalidate'
     return response
-            
-
-    # return render(request,'login.html')
+    
+    
+    
+#patient signup
 def signup(request):
     if request.method == "POST":
         username=request.POST.get('username')
@@ -62,16 +139,50 @@ def signup(request):
 
         if CustomUser.objects.filter(email=email).exists():
             messages.error(request, "Email already exists")
+        elif CustomUser.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
         elif password != confirmPassword:
             messages.error(request, "Passwords do not match")
         else:
-            user = CustomUser(username=username,first_name=firstname,last_name=lastname,dob=dob,email=email,role='PATIENT')  # Change role as needed
+            user = CustomUser(username=username,first_name=firstname,last_name=lastname,dob=dob,email=email,is_patient=True,role="PATIENT")  # Change role as needed
             user.set_password(password)
             user.save()
             messages.success(request, "Registered successfully")
             return redirect("login")
     return render(request,'signup.html')
 
+
+#doctor signup
+def signup1(request):
+    if request.method == "POST":
+        username=request.POST.get('username')
+        #fullname = request.POST.get('firstname')
+        firstname=request.POST.get('firstname') 
+        lastname = request.POST.get('lastname')
+        dob = request.POST.get('dob')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirmPassword = request.POST.get('confirmPassword')
+       # phone_number = request.POST.get('phoneNumber')
+        #address = request.POST.get('address')
+      
+        
+
+        if CustomUser.objects.filter(email=email).exists():
+            messages.error(request, "Email already exists")
+        elif CustomUser.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+        elif password != confirmPassword:
+            messages.error(request, "Passwords do not match")
+        else:
+            user = CustomUser(username=username,first_name=firstname,last_name=lastname,dob=dob,email=email,is_doctor=True,role="DOCTOR")  # Change role as needed
+            user.set_password(password)
+            user.save()
+            messages.success(request, "Registered successfully")
+            return redirect("login")
+    return render(request,'signup1.html')
+
+@never_cache
 def logout_confirmation(request):
     return render(request, 'logout_confirmation.html')
 def logout(request):
@@ -85,7 +196,7 @@ def ChangePassword(request, token):
         profile_obj = CustomUser.objects.filter(forget_password_token=token).first()
 
         if profile_obj is None:
-            messages.success(request, 'Invalid token.')
+            messages.error(request, 'Invalid token.')
             return redirect('/forget-password/')
 
         if request.method == 'POST':
@@ -93,20 +204,25 @@ def ChangePassword(request, token):
             confirm_password = request.POST.get('reconfirm_password')
 
             if new_password != confirm_password:
-                messages.success(request, 'Passwords do not match.')
+                messages.error(request, 'Passwords do not match.')
                 return redirect(f'/change-password/{token}/')
 
             # Update the password for the user associated with profile_obj
             profile_obj.set_password(new_password)
             profile_obj.forget_password_token = None  # Remove the token
             profile_obj.save()
-            return redirect('/login/')
+
+            # Set a success message
+            messages.success(request, 'Password changed successfully.')
+
+            # Redirect to login page with the success message
+            return redirect('/login/', {'success_message': 'Password changed successfully'})
 
     except Exception as e:
         print(e)
+        messages.error(request, 'An error occurred while processing your request.')
     
     return render(request, 'change-password.html', context)
-
 
 import uuid
 def ForgetPassword(request):
@@ -131,7 +247,8 @@ def ForgetPassword(request):
         print(e)
     
     return render(request, 'forget-password.html')
-
+# @login_required
+@never_cache
 def patient_profile(request):
     patient = request.user
     
@@ -143,3 +260,112 @@ def patient_profile(request):
         form = PatientProfileForm(instance=patient)
 
     return render(request, 'patient_profile.html', {'patient': patient, 'form': form})
+
+@login_required
+def admind(request):
+    # Query all User objects (using the custom user model) from the database
+    User = get_user_model()
+    user_profiles = User.objects.all()
+    
+    # Pass the data to the template
+    context = {'user_profiles': user_profiles}
+    
+    # Render the HTML template
+    return render(request, 'admind.html', context)
+
+def toggle_active(request, user_id, is_active):
+    try:
+        user = CustomUser.objects.get(id=user_id)
+        # Convert the "is_active" parameter to a boolean
+        is_active = is_active.lower() == 'true'
+        # Toggle the active status
+        user.is_active = not is_active
+        user.save()
+        messages.success(request, f'User is now {"Active" if user.is_active else "Inactive"}.')
+    except CustomUser.DoesNotExist:
+        messages.error(request, 'User not found.')
+    return redirect('admind')  # Replace 'admind' with your actual admin dashboard URL name
+
+
+
+def google_authenticate(request):
+    # Handle the Google OAuth2 authentication process
+    # ...
+
+    # After successful authentication, create or get the user
+    try:
+        user_social = UserSocialAuth.objects.get(provider='google-oauth2', user=request.user)
+        user = user_social.user
+    except UserSocialAuth.DoesNotExist:
+        user = request.user
+
+    # Set a default role for users signing in with Google (e.g., "Patient")
+    user.role = 'Patient'
+    user.save()
+
+    # Redirect to the desired page (phome.html for Patient role)
+    if user.role == 'Patient':
+        return redirect('phome')  # Make sure you have a URL named 'phome'
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+# def login(request):
+#     if request.method == "POST":
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+
+#         user = authenticate(request, username=username, password=password)
+
+#         if user is not None:
+#             auth_login(request, user)
+#             request.session['username'] = username
+#             messages.success(request, "Login successful!")
+#             return redirect("phome")  # Replace 'phome' with the name of your home page URL
+#         else:
+#             messages.error(request, "Invalid login credentials")
+
+#     response = render(request, 'login.html')
+#     response['Cache-Control'] = 'no-store, must-revalidate'
+#     return response
+            
+
+    # return render(request,'login.html')
+
+
+
+# try email login
+# from django.contrib import messages
+
+# def login(request):
+#     if request.method == 'POST':
+#         email = request.POST.get('email')
+#         password = request.POST.get('password')
+
+#         try:
+#             # Attempt to retrieve the user by email
+#             user = CustomUser.objects.get(email=email)
+
+#             # Check the password
+#             if user and check_password(password, user.password):
+#                 request.session['email'] = email
+#                 auth_login(request, user)
+#                 return redirect('phome')
+#             else:
+#                 error_message = "Invalid credentials"
+#                 messages.error(request, error_message)      
+#         except CustomUser.DoesNotExist:
+#             # User with the given email does not exist
+#             error_message = "User with this email does not exist"
+#             messages.error(request, error_message)
+    
+#     response = render(request, 'login.html')
+#     response['Cache-Control'] = 'no-store, must-revalidate'
+#     return response
+
